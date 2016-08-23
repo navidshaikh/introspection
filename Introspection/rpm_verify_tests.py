0fb66d4ee72220b396638c87f723598326f59447
Navid Shaikh <nshaikh@redhat.com>
2016-08-23 20:30:53 +0530

    Updates rpm verify test

--- Introspection/rpm_verify_tests.py
+++ Introspection/rpm_verify_tests.py
@@ -1,13 +1,3 @@
-# Copyright 2014 Red Hat Inc.
-# Navid Shaikh <nshaikh@redhat.com>
-#
-# This program is a free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the license, or(at your option) any
-# later version. See http://www.gnu.org/copyleft/gpl.html for the full text of
-# the license.
-#
-
 import os
 import json
 import re
